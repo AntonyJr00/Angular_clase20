e3e6a3bfd8eac341ad1826e9394ce006d9fa7f9c
AntonyJr00 <antonyrodl20@gmail.com>
2023-01-19 13:57:20 -0500

    interceptor agregado 2

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -17,6 +17,7 @@ export class InterceptorService implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    console.log('paso por aqui la peticion http');
     return next.handle(req);
   }
 }
